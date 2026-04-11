45532e2f4e46f5b1d4b6f0bb59a73ba606fca1c4
Mista-Log <oloyedeibrahimsmile@gmail.com>
2026-04-11 08:45:42 +0100

    Uncomment API router import and inclusion in main.py

--- GreenPal Backend/app/main.py
+++ GreenPal Backend/app/main.py
@@ -1,16 +1,17 @@
-# from api.router import api_router
+from api.router import api_router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
-# app.include_router(api_router, prefix="/api/v1")
+app.include_router(api_router, prefix="/api/v1")
 
 
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://green-pal-puce.vercel.app/",
 ]
 
 app.add_middleware(
